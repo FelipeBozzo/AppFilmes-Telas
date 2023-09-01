a6871e60313a729dd9bbaad42e42d8be65ebcb5f
Felipe <felipe.araujo143@etec.sp.gov.br>
2023-09-01 17:24:29 -0300

    Firts

---
+++ home/index.js
@@ -0,0 +1,64 @@
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import Header from '../../../components/header';
+import Search from '../../../components/searchBar';
+import BannerMovies from '../../../components/bannerMovies';
+import Filmes from '../../../data/filmes'
+import Series from '../../../data/series'
+import CardSeries from '../../../components/cardSeries';
+import CardFilmes from '../../../components/cardFilmes';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Header></Header>
+      <Search></Search>
+      <BannerMovies></BannerMovies>
+
+<View style={{width:'90%'}}>  
+      <FlatList 
+      showsHorizontalScrollIndicator = {false}
+      horizontal = {true}
+      data ={Filmes}
+      keyExtractor = {(item) => item.id}
+      renderItem = { ({item}) =>(
+
+        <CardFilmes
+          titulo = {item.nome}
+          imagem = {item.imagem}
+          nota = {item.notas}
+        ></CardFilmes>
+
+      )}
+      />
+      </View>
+
+
+      <View style={{width:'90%'}}>  
+      <FlatList 
+      showsHorizontalScrollIndicator = {false}
+      horizontal = {true}
+      data ={Series}
+      keyExtractor = {(item) => item.id}
+      renderItem = { ({item}) =>(
+
+        <CardSeries
+          titulo = {item.nome}
+          imagem = {item.imagem}
+          nota = {item.notas}
+        ></CardSeries>
+
+      )}
+      />
+      </View>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#141A29',
+    alignItems: 'center',
+  },
+});
---
+++ src/data/series.js
@@ -0,0 +1,74 @@
+const DATA =
+[
+    {
+        id:"01",
+        nome:"WandaVisions",
+        notas:'8,5',
+        imagem:'Wanda.jpg'
+    },
+
+    {
+        id:"02",
+        nome:"Bungo stray dogs",
+        notas:'9,6',
+        imagem:'bungo.jpeg'
+    },
+
+    {
+        id:"03",
+        nome:"One Piece",
+        notas:'10',
+        imagem:'one-piece.jpeg'
+    },
+
+    {
+        id:"04",
+        nome:"O Incrível Mundo de Gumbal",
+        notas:'9,9',
+        imagem:'gumbal.jpg'
+    },
+
+    {
+        id:"05",
+        nome:"Irmão do Jorel",
+        notas:'8,5',
+        imagem:'jorel.jpg'
+    },
+
+    {
+        id:"06",
+        nome:"The Office",
+        notas:'8,2',
+        imagem:'the-office.jpg'
+    },
+
+    {
+        id:"07",
+        nome:"Modern Family",
+        notas:'8,7',
+        imagem:'family.jpg'
+    },
+
+    {
+        id:"08",
+        nome:"Supernatural",
+        notas:'7,8',
+        imagem:'supernatural.jpg'
+    },
+
+    {
+        id:"09",
+        nome:"American Horror Story",
+        notas:'8,4',
+        imagem:'horror.jpg'
+    },
+
+    {
+        id:"10",
+        nome:"Vikings",
+        notas:'7,2',
+        imagem:'vikings.jpg'
+    }
+]
+
+export default DATA;
